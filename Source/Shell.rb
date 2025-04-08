aa3080e636af93c876bf244d351656f026c01d0d
Daniel Perez <cedrobit@outlook.com>
2025-04-08 15:45:08 -0400

    Update Shell.rb

--- Source/Shell.rb
+++ Source/Shell.rb
@@ -11,5 +11,15 @@ shellname = "CedroShell"
 # Show Intro Message.
 sleep(0.020)
 system('clear')
+
 sleep(0.020)
 puts "~ ${shellname} ~"
+sleep(0.020)
+puts "A useless shell designed for personal learning."
+sleep(0.020)
+puts ""
+sleep(0.020)
+puts "Written by Daniel Perez."
+
+sleep(0.720)
+puts ""
