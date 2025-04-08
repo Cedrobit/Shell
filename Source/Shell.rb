c9d1869f2a4114e628696a39e22e33d94c73d825
Daniel Perez <cedrobit@outlook.com>
2025-04-08 15:39:19 -0400

    Update Shell.rb

--- Source/Shell.rb
+++ Source/Shell.rb
@@ -1,4 +1,15 @@
-repeat = true
-while repeat == true
-  puts "This isn't the shell yet!"
-end
+# "CedroShell"
+# "Written by Daniel Perez."
+# "Licensed under MIT."
+
+
+# Set Variables.
+version = "0.0.0"
+interactive = true
+shellname = "CedroShell"
+
+# Show Intro Message.
+sleep(0.020)
+system('clear')
+sleep(0.020)
+puts "~ ${shellname} ~"
